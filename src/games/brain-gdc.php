9b6717d1c3be15891ffc89283dcbe25ce5416649
Andrew Pisarevsky <renakdup@gmail.com>
2019-12-28 23:20:34 +0300

    Рефакторинг кода игр...
    Анонимные функции были были вынесены из анонимки "$get_question_data()", т.к. это приводило к пересозданию кода в цикле "gameInterface()"

--- src/games/brain-gdc.php
+++ src/games/brain-gdc.php
@@ -4,26 +4,25 @@ use function BrainGames\core\gameInterface;
 
 $instructions = "Find the greatest common divisor of given numbers";
 
-$get_question_data = function () {
+$get_gdc = function ($number1, $number2) {
+    while ($number1 !== 0 && $number2 !== 0) {
+        if ($number1 > $number2) {
+            $number1 = $number1 % $number2;
+        } else {
+            $number2 = $number2 % $number1;
+        }
+    }
 
+    return $number1 + $number2;
+};
+
+$get_question_data = function () use ($get_gdc) {
     $number1 = rand(1, 100);
     $number2 = rand(1, 100);
 
     $question = $number1 . " " . $number2;
 
-    $gdc_result = function ($number1, $number2) {
-        while ($number1 !== 0 && $number2 !== 0) {
-            if ($number1 > $number2) {
-                $number1 = $number1 % $number2;
-            } else {
-                $number2 = $number2 % $number1;
-            }
-        }
-
-        return $number1 + $number2;
-    };
-
-    $true_answer = $gdc_result($number1, $number2);
+    $true_answer = $get_gdc($number1, $number2);
 
     return [
         'question'     => $question,
